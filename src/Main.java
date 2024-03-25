a086e2b4ed9894449e88bc82b262bde077a37af0
Gedeon-42 <umwizerwagedeon@gmail.com>
2024-03-25 17:49:48 +0200

    added calculator functionalities

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,41 @@
-import java.text.NumberFormat;
 import java.util.Scanner;
 
-public class calcultor {
-    public static void main(String[] args){
-        final byte Months_in_year=12;
-        final byte PERCENT =100;
-        Scanner scanner= new Scanner(System.in);
-        System.out.println("Principal:");
-        int principal= scanner.nextInt();
-        System.out.println("Annual Interest Rate:");
-        float annualInterest=scanner.nextFloat();
-        float monthlyInterest=annualInterest/PERCENT/Months_in_year;
-        System.out.println("Period (Years): ");
-        byte years =scanner.nextByte();
-        int numberOfPayments=years*Months_in_year;
+public class Main {
 
-        double mortage = principal *
-                (monthlyInterest*Math.pow(1+monthlyInterest,numberOfPayments))
-                /(Math.pow(1+monthlyInterest,numberOfPayments)-1);
-        String mortgegeFormatted = NumberFormat.getNumberInstance().format(mortage);
-        System.out.println("Mortage: "+mortgegeFormatted);
+    public static void main(String[] args){
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+        int principal = 0;
+        float monthlyInterst = 0;
+        int numberOfPayments = 0;
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.print("Principal:");
+             principal = scanner.nextInt();
+            if(principal >=1000 && principal <=1_000_000)
+                break;
+            System.out.print("Enter a Value between 1000 and 100000");
+        }
+        while(true) {
+            System.out.print("Annual Interest Rate:");
+            float annualInterest = scanner.nextFloat();
+            if(annualInterest >=1 & annualInterest <=30) {
+                 monthlyInterst = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.print("Enter a value bewtween 1 and 30");
+
+        }
+        while (true) {
+            System.out.print("period(years):");
+            byte years = scanner.nextByte();
+            if(years >=1 && years <=30) {
+                 numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("enter value 1 and 30");
+        }
+        double mortage = principal
+                *(monthlyInterst * Math.pow(1+monthlyInterst)) /(Math.pow(1 +monthlyInterst,numberOfPayments));
     }
 }
